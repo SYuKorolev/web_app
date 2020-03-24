17a7669517669fd6fdccdabde12867a7c27cb991
Sergey Korolev <korolev@edu.misis.ru>
2020-03-24 19:51:29 +0300

    initial commit

---
+++ web_app/trend_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+class Description(models.Model):
+    graph_name = models.CharField(max_length=255)
+    x_axis_name = models.CharField(max_length=255)
+    axis_name = models.CharField(max_length=255)
+
+class Trend(models.Model):
+    json = JSONField()
+    desc = models.ForeignKey(
+        Description,
+        on_delete=models.CASCADE,
+        related_name='trend'
+    )
